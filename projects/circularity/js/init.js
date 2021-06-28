f2082bc0e821bb369caaa8ce6d958f6ab3420360
Jackson-Hoyt <jacksonhhoyt@gmail.com>
2021-06-28 17:16:15 +0000

    add five circles to project

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -33,10 +33,14 @@ var init = function (window) {
 
 
         // TODO 3 / 8 : Call the drawCircle() function 
-        for (var i = 0; i < 100; i++) {
-            drawCircle();        
-        }
-        
+        //for (var i = 0; i < 100; i++) {
+        //    drawCircle();        
+        //}
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
