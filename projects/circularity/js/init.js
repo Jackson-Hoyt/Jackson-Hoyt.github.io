c74d180a20857dd7a9bbf27bc67e0453913900b6
Jackson-Hoyt <jacksonhhoyt@gmail.com>
2021-06-28 17:33:48 +0000

    add five circles to project

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -55,19 +55,29 @@ var init = function (window) {
             // TODO 4 : Update the circle's position //
             
             // Following for loop updates the circles position.
-   
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
             
             // Following for loop calls the game.checkCirclePosition() function.
+            game.checkCirclePosition(circles[0]);
+            game.checkCirclePosition(circles[1]);
+            game.checkCirclePosition(circles[2]);
+            game.checkCirclePosition(circles[3]);
+            game.checkCirclePosition(circles[4]);
 
-            // TODO 9 : Iterate over the array
+            /* TODO 9 : Iterate over the array
             for (var i = 0; i < circles.length; i++) {
                 var eachValue = circles[i];
                 
                 physikz.updatePosition(eachCircle);
                 game.checkCirclePosition(eachCircle);
             }
+            */
             
         }
     
